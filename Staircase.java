bcd83f8cdbd1a3d975e5515d787fd3d1de5c222b
sopheareachte <34178021+sopheareachte@users.noreply.github.com>
2025-02-10 13:21:57 -0800

    Update Staircase.java

--- Staircase.java
+++ Staircase.java
@@ -20,32 +20,32 @@ public class Staircase {
 		Scanner input = new Scanner(System.in);
 	
 		//Get user input for steps in the staircase	
-	    System.out.print("How many steps in the staircase? ");
-	    int step = input.nextInt();
+		System.out.print("How many steps in the staircase? ");
+		int step = input.nextInt();
 	
 		//Get user input for the step width in cm
-	    System.out.print("Enter step width in cm: ");
-	    double width = input.nextDouble();
+		System.out.print("Enter step width in cm: ");
+		double width = input.nextDouble();
 	        
-	    //Get user input for the step rise in cm
-	    System.out.print("Enter rise of the step in cm: ");
-	    double rise = input.nextDouble();
+		//Get user input for the step rise in cm
+		System.out.print("Enter rise of the step in cm: ");
+		double rise = input.nextDouble();
 	        
-	    //Get user input for the step run in cm
-	    System.out.print("Enter run of the step in cm: ");
-	    double run = input.nextDouble();
+		//Get user input for the step run in cm
+		System.out.print("Enter run of the step in cm: ");
+		double run = input.nextDouble();
 	        
-	    //Calculate the volume of concrete necessary to build a staircase
-	    int blocks = (step * (step + 1)) / 2;
-	    double blockVolume = width * rise * run;
+		//Calculate the volume of concrete necessary to build a staircase
+		int blocks = (step * (step + 1)) / 2;
+		double blockVolume = width * rise * run;
 		double staircaseVolume = blocks * blockVolume;
-	    int volumeRounded = (int) (Math.ceil(staircaseVolume));
+		int volumeRounded = (int) (Math.ceil(staircaseVolume));
 	        
-	    //Print the volume of concrete necessary to build a staircase
-	    System.out.printf("Total volume is %,d cubic centimeters.", 
+		//Print the volume of concrete necessary to build a staircase
+		System.out.printf("Total volume is %,d cubic centimeters.", 
 							volumeRounded);
 	        
-	    input.close();
+		input.close();
         
 	}
 }
